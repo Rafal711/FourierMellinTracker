a5c2a759e1c2bf1977eaa5c5f2ec9589c18e0640
Rafal Guzek <rafaguzek@gmail.com>
2022-10-28 16:33:01 +0200

    added comparing methods

---
+++ FourierMellinTracker/compareImgs.py
@@ -0,0 +1,12 @@
+import numpy as np
+import filters
+from skimage.metrics import structural_similarity
+
+
+def mse(imageA, imageB):
+    error = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    error /= float(imageA.shape[0] * imageA.shape[1])
+    return error
+
+def ssim(imageA, imageB):
+    return structural_similarity(imageA, imageB)
